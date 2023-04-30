985f5f9388379dec863b7c9490dfca6e67e97411
solovmon <solomonvlad31@gmail.com>
2023-04-30 19:18:38 +0300

    Further rework of structure

--- scripts/background.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-chrome.webNavigation.onBeforeNavigate.addListener((details) =>{
-    console.log(details.url);
-    if (details.url.includes('twitter.com')){
-        console.log('xd');
-    }
-});
-  
